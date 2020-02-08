e1bd611f51861d49c7a7bc0d0ee4184b5578ef70
Kavita Bane <kavitabane123@gmail.com>
2020-02-07 23:30:27 -0800

    changes

--- src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -51,7 +51,8 @@ public class WebSampleTest extends AbstractTest {
     @TestTag(name = "specialization", value = "xlsx")
     @XlsDataSourceParameters(path = "xls/demo.xlsx", sheet = "GSMArena", dsUid = "TUID", dsArgs = "brand, model, display, camera, ram, battery")
     public void testModelSpecs(String brand, String model, String display, String camera, String ram, String battery) {
-        // Open GSM Arena home page and verify page is opened
+        //Feature Web Sample Test 
+    	// Open GSM Arena home page and verify page is opened
         HomePage homePage = new HomePage(getDriver());
         homePage.open();
         Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
